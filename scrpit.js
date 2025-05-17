62abe187329f3ea9425e536b0c0f0782bbefde11
Diogo-Almeida-Costa <jdiogoalmeida988@gmail.com>
2025-05-17 16:37:29 -0300

    Criando forms

---
+++ scrpit.js
@@ -0,0 +1,26 @@
+document.addEventListener('CarregarDOM', 
+    () => 
+    {
+        const form = document.getElementById('task-form');
+
+        form.addEventListener('submit',
+            (e) =>
+            {
+                e.preventDefault();
+
+                const task = {
+                    title: document.getElementById('title').value,
+                    date: document.getElementById('date').value,
+                    comment: document.getElementById('comment').value,
+                    priority: document.getElementById('priority').value,
+                    notify: document.getElementById('notify').checked,
+                    createdAt: new Date().toISOString()
+                };
+
+                console.log('Tarefa cadastrada: ', task);
+
+                form.reset();
+            }
+        );
+    }
+);
