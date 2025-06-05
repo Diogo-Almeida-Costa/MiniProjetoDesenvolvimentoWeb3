92eebd1d6eca3e526d97812287982c264d5754f0
Diogo-Almeida-Costa <jdiogoalmeidac988@gmail.com>
2025-06-05 09:18:32 -0300

    Atualização no service

--- js/service/TarefaService.js
+++ js/service/TarefaService.js
@@ -74,6 +74,51 @@ export class TarefaService
 
     async updateTarefa(tarefaId, dadoAlterado)
     {
-        const url = ``;
+        const url = `https://tarefa-desenvolvimento-web-default-rtdb.firebaseio.com/tarefas/tarefas/${tarefaId}.json`;
+
+        try
+        {
+            const resposta = await fetch(url, {
+                method: "PUT",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(dadoAlterado),
+            });
+
+            if(!resposta.ok)
+            {
+                throw new Error("Erro para atualizar tarefa");
+            }
+            return await resposta.json();
+        }
+        catch(error)
+        {
+            console.error("Erro ao atualizar tarefa:", error);
+            throw new Error("Algo deu errado ao atualizar a tarefa");
+        }
+    }
+
+    async deletarTarefa(tarefaId)
+    {
+        const url = `https://tarefa-desenvolvimento-web-default-rtdb.firebaseio.com/tarefas/${tarefaId}.json`;
+
+        try
+        {
+            const resposta = await fetch(url, {
+                method: "DELETE",
+            });
+
+            if(!resposta.ok)
+            {
+                throw new Error("Erro para remover tarefa");
+            }
+            return true;
+        }
+        catch(error)
+        {
+            console.error("Erro para deletar tarefa:", error);
+            throw new Error("Algo deu errado para deletar a tarefa");
+        }
     }
 }
